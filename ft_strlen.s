b59c56bd5eb33d0060c28b53e9cca5d4df7deae1
eprei <93380002+eprei@users.noreply.github.com>
2025-07-03 10:45:57 +0200

    ft_strlen format improvement

--- ft_strlen.s
+++ ft_strlen.s
@@ -3,13 +3,16 @@ section .text
 
 ft_strlen:
         mov rbx, 0      ; initialize length counter to 0
+
 loop:
         mov cl, [rdi]   ; load the current character
         cmp cl, 0       ; check if it is the null terminator
         je exit_loop    ; if it is, exit the loop
+
         inc rdi         ; increment pointer to next character
         inc rbx         ; increment length counter
         jmp loop        ; continue looping
+
 exit_loop:
         mov rdi, rbx    ; move the length into rdi for return value
         mov rax, rbx    ; move the length into rax for return value
